7c385c51800740721a19183990c41be65a4c9eba
Reza Aghajani <rezaaghajani@protonmail.com>
2022-11-24 07:11:48 +0330

    Creating Telegram Bot

--- bot.js
+++ bot.js
@@ -1,7 +1,26 @@
 const fs = require('fs')
 const ytdl = require('ytdl-core')
+const TelegramBot = require("node-telegram-bot-api");
+require('dotenv').config()
 
-ytdl("http://www.youtube.com/watch?v=aqz-KE-bpKQ",{quality: '18'}).pipe(
-  fs.createWriteStream("video2.mp4")
-);
+const token = process.env.token
+const bot = new TelegramBot(token, {polling:true})
+
+bot.onText(/\/video (.+)/, (msg, match) => {
+    const chatId = msg.chat.id
+    const videoURL = match[1]
+    async () => {
+        ytdl(videoURL, { quality: "18" }).pipe(
+            await fs.createWriteStream("video2.mp4")
+        )
+    }
+    bot.sendVideo(chatId,"video2.mp4")
+
+})
+
+
+
+// ytdl("http://www.youtube.com/watch?v=aqz-KE-bpKQ",{quality: '18'}).pipe(
+//   fs.createWriteStream("video2.mp4")
+// );
 
